186214365da46df0d1524bf7686eefb04f814eb1
ffwATL <4else@i.ua>
2015-11-18 15:09:32 +0200

    fixed pom file and optimized usage of resources

--- src/main/java/root/view/SceneView.java
+++ src/main/java/root/view/SceneView.java
@@ -21,7 +21,7 @@ public class SceneView extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(new File("src/main/java/root/controller/scene.fxml").toURI().toURL()); //for testing with IDEA
-        Image ico = new Image("file:src/main/resources/ico.png");  //for testing with IDEA
+        Image ico = new Image("file:src/main/java/root/view/fxml/icon.png");  //for testing with IDEA
         /*AnchorPane root = (AnchorPane) FXMLLoader.load(SceneView.class.getResource("fxml/scene.fxml")); //for deployment
         Image ico = new Image(this.getClass().getResource("fxml/icon.png").toExternalForm()); //for deployment*/
         Scene scene = new Scene(root);
