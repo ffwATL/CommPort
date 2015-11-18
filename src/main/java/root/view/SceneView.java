269c95f04840e1d2e292c58ffa3271169cb3030e
ffwATL <4else@i.ua>
2015-11-18 21:22:02 +0200

    changed skin

--- src/main/java/root/view/SceneView.java
+++ src/main/java/root/view/SceneView.java
@@ -3,12 +3,14 @@ package root.view;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import root.model.JsscCommModel;
+
+import java.io.File;
 
 public class SceneView extends Application {
 
@@ -18,10 +20,10 @@ public class SceneView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*Parent root = FXMLLoader.load(new File("src/main/java/root/controller/scene.fxml").toURI().toURL()); //for testing with IDEA
-        Image ico = new Image("file:src/main/java/root/view/fxml/icon.png");  //for testing with IDEA*/
-        AnchorPane root = (AnchorPane) FXMLLoader.load(SceneView.class.getResource("fxml/scene.fxml")); //for deployment
-        Image ico = new Image(this.getClass().getResource("fxml/icon.png").toExternalForm()); //for deployment
+        Parent root = FXMLLoader.load(new File("src/main/java/root/controller/scene.fxml").toURI().toURL()); //for testing with IDEA
+        Image ico = new Image("file:src/main/java/root/view/fxml/icon.png");  //for testing with IDEA
+        /*AnchorPane root = (AnchorPane) FXMLLoader.load(SceneView.class.getResource("fxml/scene.fxml")); //for deployment
+        Image ico = new Image(this.getClass().getResource("fxml/icon.png").toExternalForm()); //for deployment*/
         Scene scene = new Scene(root);
         primaryStage.getIcons().add(ico);
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
